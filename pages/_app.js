31fa42418d782563bf9205209b2249d42afd2e20
jtraut1996 <102989547+jtraut1996@users.noreply.github.com>
2022-11-17 07:21:39 -0800

    added in google analytics dataLayer

--- pages/_app.js
+++ pages/_app.js
@@ -21,6 +21,21 @@ const MyApp = ({ Component, pageProps }) => {
   const [currentIdInView, setCurrentIdInView] = useState(0);
   return (
     <StateContext>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <Head>
         <link rel="apple-touch-icon" href="/"></link>
         <title>{"landypandyy"}</title>
